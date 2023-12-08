89562ff2ffe46b2556f18b05d48a89f745090077
granth2023 <grantharris38@gmail.com>
2023-12-08 10:30:38 -0500

    app dot get added to server file

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port {PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my portfolio server');
+});
