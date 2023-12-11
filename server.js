445560b2c7037d1419a78b9c9ed1e03e0577bae0
granth2023 <grantharris38@gmail.com>
2023-12-11 10:33:06 -0500

    server js updated for middleware intialization

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const handle = nextApp.getRequestHandler();
 const app = express();
 const PORT = 3001;
 
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 
 
