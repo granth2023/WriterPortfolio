b1c05785687735019ba8255709bc608c51689259
granth2023 <grantharris38@gmail.com>
2023-12-11 10:50:51 -0500

    three js basic setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,2 @@
 import React from "react";
-
-const Home: React.FC = () => {
-  return (
-    <div>
-      <h1> Welcome to My Portfolio</h1>
-    </div>
-  );
-};
-
-export default Home;
+import ThreeContainer from 
