b5fdd34ba1401a0d36877d37879f3156e3ae3d84
granth2023 <grantharris38@gmail.com>
2023-12-11 11:01:30 -0500

    index js set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,13 @@
 import React from "react";
-import ThreeContainer from 
+import ThreeContainer from '../../WriterPortfolio/writer-portfolio/src/app/components/ThreeContainer'
+
+const Home = () => {
+  return ( 
+    <div> 
+      <h1> Welcome to my Three.JS integration</h1>
+      <ThreeContainer/>
+    </div>
+  );
+};
+
+export default Home;
