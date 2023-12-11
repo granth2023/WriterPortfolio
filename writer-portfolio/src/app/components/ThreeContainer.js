2a502349d5e5343377d6bea75ccd2575c45dfae6
granth2023 <grantharris38@gmail.com>
2023-12-11 10:52:20 -0500

    simple object cube added

--- writer-portfolio/src/app/components/ThreeContainer.js
+++ writer-portfolio/src/app/components/ThreeContainer.js
@@ -13,3 +13,9 @@ const ThreeContainer = () => {
         
         
         //add a simple object cube)
+        const geometry = new THREE.BoxGeometry();
+        const material  = new THREE.MeshBasicMaterial({ color: 0x00ff00});
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube));
+
+        camera.position.z = 5;
