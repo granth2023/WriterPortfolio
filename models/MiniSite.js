cf5837b54f0f2edd80b72ca586710bf129e36b54
granth2023 <grantharris38@gmail.com>
2023-12-12 08:33:41 -0500

    models begin

---
+++ models/MiniSite.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const fileSchema = new mongoose.Schema({
+    url: String, 
+    isProtected: Boolean, 
+    password: String
+});
+
+const miniSiteSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    content: [{ type: Map, of: String }],
+    files: [fileSchema],
