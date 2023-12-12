367bd432476f140b3c8a840ddb442064a5d7069d
granth2023 <grantharris38@gmail.com>
2023-12-12 10:33:47 -0500

    controllers addded

--- controllers/miniSiteControllers.js
+++ controllers/miniSiteControllers.js
@@ -1,4 +1,31 @@
 import MiniSite from '../models/MiniSite';
 
 export const createMiniSite = async (req, res) => {
+    try{
+        const miniSite = new MiniSite(req.body);
+        const savedMiniSite = await miniSite.save();
+        res.status(201).json(savedMiniSite);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 };
+
+export const getMiniSites = async (req, res) => {
+    try {
+        const miniSites = await MiniSite.find();
+        res.json(miniSites);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+};
+};
+
+export const getMiniSiteById = async (req, res) => {
+    try {
+        const miniSite = await MiniSite.findById(req.params.id);
+        if(!miniSite) return res.status(404).json({ message: 'MiniSite not found'});
+        res.json(miniSite);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
