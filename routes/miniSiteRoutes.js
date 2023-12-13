c0ed978c7c8545874a703f26ec1e654e80cf703e
granth2023 <grantharris38@gmail.com>
2023-12-13 12:09:48 -0500

    routes beginnign to work

--- routes/miniSiteRoutes.js
+++ routes/miniSiteRoutes.js
@@ -9,10 +9,10 @@ import {
 
 const router = express.Router();
 
-router.post('/api/minisites', createMiniSite);
-router.get('/api/minisites', getMiniSites);
-router.get('/api/minisites/:id', getMiniSiteById);
-router.put('/api/minisites/:id', updatedMiniSite);
-router.delete('/api/minisites/:id', deleteMiniSite);
+router.post('/', createMiniSite);
+router.get('/', getMiniSites);
+router.get('/:id', getMiniSiteById);
+router.put('/:id', updatedMiniSite);
+router.delete('/:id', deleteMiniSite);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -48,13 +48,48 @@
 
 
 import express from 'express';
+import next from 'next';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import router from '../routes/miniSiteRoutes.js';
+
+dotenv.config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
 const app = express();
 const PORT = 3001;
 
+const dbURI = process.env.MONGODB_URI; 
+mongoose.connect(dbURI)
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.error('Could not connect to MongoDB:', err));
+
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+
 app.get('/api/test', (req, res) => {
     res.send('Test route is working');
 });
+
+app.use('/api/minisites', router);
+
+nextApp.prepare().then(() => {
+    app.get('*', (req, res) => {
+        return handle(req, res);
+    });
+   
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
