18d6f14dc43226a8c069377c23eec1f3e015a618
granth2023 <grantharris38@gmail.com>
2023-12-14 12:30:42 -0500

    if statement for if bad response

--- services/miniSiteService.ts
+++ services/miniSiteService.ts
@@ -3,6 +3,9 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL
 export const fetchMiniSites = async (): Promise<void> => {
     try{ 
         const response = await fetch(`${API_URL}/api/miniSites`);
+        if(!response.ok){
+            
+        }
 
     } catch (error) { 
         console.error('Error fetching mini sites:', error);
