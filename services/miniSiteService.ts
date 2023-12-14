c5438fd78b5f0455c86d0ffb818f6ca104a21250
granth2023 <grantharris38@gmail.com>
2023-12-14 12:30:18 -0500

    response variable created for the fetch

--- services/miniSiteService.ts
+++ services/miniSiteService.ts
@@ -1,7 +1,8 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const fetchMiniSites = async (): Promise<void> => {
-    try{
+    try{ 
+        const response = await fetch(`${API_URL}/api/miniSites`);
 
     } catch (error) { 
         console.error('Error fetching mini sites:', error);
