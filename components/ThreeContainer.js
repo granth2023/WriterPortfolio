ec3fa8839a82600c0cf7c1a98c4ea3cb50b08857
granth2023 <grantharris38@gmail.com>
2023-12-15 11:38:52 -0500

    ambient light added to scene

--- components/ThreeContainer.js
+++ components/ThreeContainer.js
@@ -13,6 +13,8 @@ const ThreeContainer = () => {
 
         //Lighting effect
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+        scene.add(ambientLight);
+        
 
         
         
