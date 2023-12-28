ca3fe00bdb937514bd9dd83a022c91c73ab41e88
granth2023 <grantharris38@gmail.com>
2023-12-28 01:44:07 -0500

    placeholder red

---
+++ components/ImageBox.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+interface ImageBoxProps {
+    imageUrl: string;
+    altText: string;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
-import React from "react";
-import ThreeContainer from '../components/ThreeContainer'
+// import React from "react";
+// import ThreeContainer from '../components/ThreeContainer'
 
-const Home = () => {
+// const Home = () => {
+//   return ( 
+//     <div> 
+//       <h1> Welcome to my Three.JS integration</h1>
+//       <ThreeContainer/>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import ImageBox from '../components/ImageBox';
+
+const HomePage = () => {
   return ( 
-    <div> 
-      <h1> Welcome to my Three.JS integration</h1>
-      <ThreeContainer/>
+    <div className="flex flex-wrap justify-center items-center bg-gray-100 p-8">
+      {[1, 2, 3, 4, 5].map((num) => {
+        <ImageBox
+          key={num}
+          altText={'Placeholder Image $Pindex + 1}`}
+          />
+      ))}
+     
     </div>
-  );
-};
-
-export default Home;
+  )
+      };
