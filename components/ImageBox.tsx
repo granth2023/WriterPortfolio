7ed165fb697866489fbdf62974b39cce020621ce
granth2023 <grantharris38@gmail.com>
2023-12-28 01:46:02 -0500

    url code ready to go

--- components/ImageBox.tsx
+++ components/ImageBox.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-interface ImageBoxProps {
-    imageUrl: string;
-    altText: string;
-}
+// interface ImageBoxProps {
+//     imageUrl: string;
+//     altText: string;
+// }
+
+// const ImageBox: React.FC<ImageBoxProps> = ({ imageUrl, altText }) => {
+//     return (
+//         <div className="m-4 shadow-lg border-4 border-gray-800 bg-white">
+//             <img src={imageUrl} alt={altText} className="w-full" />
+//         </div>
+//     );
+// };
+// export default ImageBox;
