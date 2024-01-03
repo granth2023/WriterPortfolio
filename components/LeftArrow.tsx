d8de7adf006b3505f16f9795175cf89c15510ca6
granth2023 <grantharris38@gmail.com>
2024-01-03 16:58:16 -0500

    left arrow function begun to push onto bluepage

---
+++ components/LeftArrow.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router';
+
+const LeftArrow = () => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push('/BluePage');
+    };
+    return (
+        <div className=
+    )
+}
